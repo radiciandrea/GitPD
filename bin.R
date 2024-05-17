8bcf1daeef2c89787655ae0647726d1ecb505850
radiciandrea <andrea.radici93@libero.it>
2024-05-17 13:25:10 +0200

    parameters declared

--- bin.R
+++ bin.R
@@ -1,1 +1,12 @@
 # Little 2 pop epidemic model
+
+#parameters
+
+gamma = 2 #fertility rate mosquitoes: 4 mosquitoes/day, sex ratio 1:1
+mu_j = 1/30 #mortality rate eggs/juveniles
+K = 1000  # carrying capacity juveniles
+delta = 0.5 # passage J to A
+mu_a = 1/50 # mortality rate mosquitoes
+beta_AsI = 0.1 # mosquito infectious rate
+beta_SAi = 0.1  # human infectious rate
+alfa = 1/14 #human recovery rate
