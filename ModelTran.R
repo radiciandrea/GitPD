b6e8f4e39e1d448f34690474b2ba7cb62f5685d4
radiciandrea <andrea.radici93@libero.it>
2024-05-22 16:56:10 +0200

    settings to simulate multiple years

--- ModelTran.R
+++ ModelTran.R
@@ -29,10 +29,14 @@ TDD_Ag = 77 # Total number of degree-days necessary for egg maturation (°C)
 t_s = 31+28+10 # Start of the favorable season - 10 Mar
 t_end = 31+28+31+30+31+30+31+31+30 # End of the favorable season - 30 Sept
 
+t_s = 1 # simulate mmmultiple year
+t_end = 365*4
+
+
 # T and P
 d = t_s:t_end
 temp = 15 - 13*cos(d/365*2*pi); # temperatura sinusoidale, min 1 gen = 2 gradi, max 1 lug = 28
-prec = temp*rep(c(0,0,0,1), 91)[1:(t_end-t_s+1)] # piove ogni 4 giorni con questa forma strana
+prec = temp*rep(c(0,0,0,1), floor(395/4))[1:(t_end-t_s+1)] # piove ogni 4 giorni con questa forma strana
 
 # p cumulated over 2 weeks and normalized between 0 and 1 (over a year?)
 p_cumm = sapply(1:length(prec), function(x){return(sum(prec[max(1,x-13):x]))})
@@ -54,7 +58,7 @@ m_A = pmax(mu_A, 0.04417 + 0.00217*temp) # Adult mortality rate (day−1)
 k_L = K_L*(p_cumm_norm+1) # Environment carrying capacity of larvae (ha−1)
 k_P = K_P*(p_cumm_norm+1) # Environment carrying capacity of pupae (ha−1)
 
-n_s = 1 # number of locations (added; 1 vor no dimension)
+n_s = 1 # number of locations (added; 1 for no dimension)
 
 # list with parameters to be passed to the ODE system
 parms = list(beta_1 = beta_1,
