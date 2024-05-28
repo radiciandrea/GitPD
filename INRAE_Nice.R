97fb879220480867dfec907f6f3e4dcf89cfc5c2
radiciandrea <andrea.radici93@libero.it>
2024-05-28 14:57:39 +0200

    Still looking for a good database for Nice

---
+++ INRAE_Nice.R
@@ -0,0 +1,19 @@
+#Climatik
+# c'è montpellier ma non c'è Nizza :(
+
+
+# https://agroclim.inrae.fr/siclima/extraction/#HomePlace:
+
+# Base climatique: SAFRAN
+# Maille Nice: 8623
+
+#no, bisogna chiedere autorizzazione
+
+#Drias
+#	Simulations 'DRIAS-2020': données quotidiennes corrigées [format Netcdf]
+# Non va bene: la "référence" arriva fino al 2005
+
+#MeteoFrance
+#https://donneespubliques.meteofrance.fr/?fond=produit&id_produit=230&id_rubrique=40
+
+# Climate data store, Copernicus
