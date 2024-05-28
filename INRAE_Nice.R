97290b2a99adcfce159dc58dc9b95b0685952e26
radiciandrea <andrea.radici93@libero.it>
2024-05-28 17:05:20 +0200

    Added data sources

--- INRAE_Nice.R
+++ INRAE_Nice.R
@@ -16,4 +16,25 @@
 #MeteoFrance
 #https://donneespubliques.meteofrance.fr/?fond=produit&id_produit=230&id_rubrique=40
 
+read.csv("C:/Users/Andrea/Desktop/Alcuni file permanenti/Post_doc/Dati/MeteoFrance/SIM2_2010_2019.csv")
+
 # Climate data store, Copernicus
+
+# SICLIMA
+
+load("C:/Users/Andrea/Desktop/Alcuni file permanenti/Dottorato/Codice/07_METAPOP_model/Data SICLIMA/tas_tasmin_pr_1980_2021.RData")
+
+# Nice 2008 2011
+
+time <- which(year %in% 2008:2011)
+cell <- 330
+
+W_tot_df <- data.frame(region = "Nice",
+                   year = year[time],
+                   DOY = jd[time],
+                   date = NA,
+                   P = pr[cell, time],
+                   T_av = tas[cell, time])
+
+W_tot_df$DOS = 1:nrow(W_tot_df)
+W_tot_df$date = as.Date(W_tot_df$DOS, origin = "2007-12-31")
