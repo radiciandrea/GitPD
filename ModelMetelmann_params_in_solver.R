b5d048c98853f031529e7016ae67a39881152fe9
radiciandrea <andrea.radici93@libero.it>
2024-06-14 13:49:04 +0200

    Added correct file weather and eggs

--- ModelMetelmann_params_in_solver.R
+++ ModelMetelmann_params_in_solver.R
@@ -14,12 +14,12 @@ library(suncalc)
 
 #Getting T and P and Eggs from Arpae (see ReadNc+ARPAE) + nc by Cyril
 
-#load("C:/Users/Andrea/Desktop/Alcuni file permanenti/Post_doc/Dati/Weather_Nice_200811.RData") #Nizza
-load("C:/Users/Andrea/Desktop/Alcuni file permanenti/Post_doc/Dati/Eggs_Weather_ER_20112021.RData") #Emilia Romagna
+load("C:/Users/Andrea/Desktop/Alcuni file permanenti/Post_doc/Dati/Eggs_Weather_Nice_200811.RData") #Nizza
+# load("C:/Users/Andrea/Desktop/Alcuni file permanenti/Post_doc/Dati/Eggs_Weather_ER_20112021.RData") #Emilia Romagna
 
 # chose a region and years
-region_x = "FERRARA" # "BOLOGNA" "PIACENZA #NICE
-year_x = 2011:2021 #2011:2021 in EMILA ROMAGNA  # 2008:2011 for Nice
+region_x = "NICE" # "BOLOGNA" "PIACENZA
+year_x = 2008:2011 #2011:2021  2008:2011 for Nice
 
 # temp and prec (by now, only at a daily step: it should change at least hour by hour)
 
