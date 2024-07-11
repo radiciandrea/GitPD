ea5a7f4aad183fba31e3facc586176c5f96137ba
radiciandrea <andrea.radici93@libero.it>
2024-07-11 14:05:11 +0200

    Adapted to aggregate raster at the resoution of weather data

--- READ_raster.R
+++ READ_raster.R
@@ -21,19 +21,36 @@ folder_r = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/GPW_
 folder_sh = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/"
 
 # load eobs gid
-domain <- st_read(paste0(folder_sh, "grid_eobs.shp")) 
+#domain <- st_read(paste0(folder_sh, "grid_eobs.shp")) 
+domain <- st_read(paste0(folder_sh, "grid_eobs_0.1.shp")) 
 
 # load gpw-v4
-pop_gpw <- raster(paste0(folder_r, "gpw_v4_population_count_rev11_2015_15_min.tif"))
+#pop_gpw <- raster(paste0(folder_r, "gpw_v4_population_count_rev11_2015_15_min.tif"))
+pop_gpw <- raster(paste0(folder_r, "gpw_v4_population_count_rev11_2015_30_sec.tif"))
+res(pop_gpw)
+
+pop_gpw_crop <- raster::crop(pop_gpw, st_bbox(domain))
+
+# writeRaster(pop_gpw_crop,paste0(folder_r, "gpw_v4_population_count_rev11_2015_30_sec_crop.tif"),options=c('TFW=YES'))
+
+# #replace NA with zeros 
+# pop_gpw[which(is.na(pop_gpw)==T)] = 0
+
+pop_gpw_agg <- aggregate(pop_gpw_crop, fact=12, fun = sum)
+
+# pop_gpw_agg[which(pop_gpw_agg==0)] = NA
+
+#aggregate raster
+writeRaster(pop_gpw_agg,paste0(folder_r, "gpw_v4_population_count_rev11_2015_01.tif"),options=c('TFW=YES'))
 
 #extract from centroids - fix crs
-pop_gpw_values <- extract(pop_gpw, st_transform(st_centroid(domain), st_crs(pop_gpw)))
+pop_gpw_values <- extract(pop_gpw_agg, st_transform(st_centroid(domain), st_crs(pop_gpw_agg)))
 
 domain <- domain %>%
   mutate(pop = pop_gpw_values) 
 
 domain$popkm2 <- domain$pop*10^6/as.numeric(st_area(domain))
 
-# st_write(domain, paste0(folder_sh, "grid_eobs+pop.shp"))
+# st_write(domain, paste0(folder_sh, "grid_eobs_01+pop.shp"))
 
 
