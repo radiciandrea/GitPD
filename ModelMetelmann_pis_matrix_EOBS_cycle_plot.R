a18392e25945be4905419ab2ee58770b3f61d6ac
radiciandrea <andrea.radici93@libero.it>
2024-07-11 14:03:00 +0200

    Plot map of Europe with E0 and E0 variation

--- ModelMetelmann_pis_matrix_EOBS_cycle_plot.R
+++ ModelMetelmann_pis_matrix_EOBS_cycle_plot.R
@@ -12,25 +12,57 @@ library(sf)
 
 folder_out = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/EOBS_sim"
 
-name = "W_EU"
-year = 2005
+# name = "W_EU"
+# year = 2005
 
-load(paste0(folder_out, "/Sim_EOBS_", name, "_", year, ".RData"))
+files = list.files(folder_out)
+
+name = substring(files[1], 10, 13)
+years = substring(files, 15, 18)
+
+#carichiamo 1 per le dimensioni
+load(paste0(folder_out, "/", files[1]))
+
+E0_m = matrix(NA, ncol = length(E0_v), nrow = length(files))
+
+for (i in 1:length(files)){
+  file = files[i]
+  load(paste0(folder_out, "/", file))
+  E0_m[i,]= E0_v
+}
+
+# correzione degli NaN con formula geomatrica
+E0_m_c <- apply(E0_m, 2, function(x){x[which(is.nan(x))] = exp(mean(log(x[which(is.nan(x)==F)]))); return(x)})
+
+#Metelamnn, geometric mean. = exp(mean(log))
+years_sel = 2017:2023 # 2017:2023 # 2006:2016
+E0_sel = apply(E0_m_c[which(years %in% years_sel),], 2, function(x){exp(mean(log(x)))})
+
+E0_diff = 100*(apply(E0_m_c[which(years %in% 2017:2023),], 2, function(x){exp(mean(log(x)))})- 
+  apply(E0_m_c[which(years %in% 2006:2016),], 2, function(x){exp(mean(log(x)))}))/
+  apply(E0_m_c[which(years %in% 2006:2016),], 2, function(x){exp(mean(log(x)))})
 
 domain_sel <- st_read(paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/domain_sel_", name, ".shp")) 
 
 br = c(0, 10^(-3:3), 10^10)
+br_diff = c(-10^c(2, log(50,10), 0), 0, 10^(0:3), 10^10)
+
+#green-yellow-red-purple
+col_br= c("#007917", "#65992E", "#8FB338", "#E2CF4D", "#F89061", "#F96970", "#F97ADC", "#A494FB")
 
 #Metelmann map
-#br = c(0, 10^(-3:3), 10^10)
-#col_br= c("#384AB9", "#4F71D4", "#8CB1FF", "#8CB1FF", "#8CB1FF", "#8CB1FF", "#8CB1FF", "#8CB1FF")
+col_br= c("#384AB4", "#5570DF", "#8EB0FE", "#C5D7F3", "#F2CDBB", "#F29878", "#D04B45", "#B00026")
 
-domain_sel <- domain_sel%>%
+domain_years_sel <- domain_sel%>%
   arrange(region) %>%
-  mutate(E0 = E0_v)%>%
+  mutate(E0 = E0_sel)%>%
   mutate(E0_level=cut(E0, breaks=br,
                       labels=sapply(br[-length(br)], function(x){paste0(">", as.character(x))}))) %>%
-  mutate(E0_level=factor(as.character(E0_level), levels=rev(levels(E0_level))))
+  mutate(E0_level=factor(as.character(E0_level), levels=rev(levels(E0_level)))) %>%
+  mutate(E0_diff = E0_diff)%>%
+  mutate(E0_diff_level=cut(E0_diff, breaks=br_diff,
+                      labels=sapply(br_diff[-length(br_diff)], function(x){paste0(">", as.character(x))}))) %>%
+  mutate(E0_diff_level=factor(as.character(E0_diff_level), levels=rev(levels(E0_diff_level))))
 
 regions_sh <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_adm/regions_2015_metropole_region.shp")
 
@@ -39,19 +71,26 @@ if (name == "Occitanie") {
     filter(Region == "Languedoc-Roussillon et Midi-P")
 }
 
-
 ggplot()+
-  geom_sf(data = domain_sel, aes(fill = E0_level))+ #
-  scale_fill_manual(values = rev(c("#007917", "#65992E", "#8FB338", "#E2CF4D", "#F89061", "#F96970", "#F97ADC", "#A494FB")))+
-  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
+  geom_sf(data = domain_years_sel, aes(fill = E0_level))+ #
+  scale_fill_manual(values = rev(col_br))+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")+
+  ggtitle(paste0("R0 diapausing eggs, period = ", min(years_sel), "-", max(years_sel)))
 # + scale_fill_gradient(trans = "log")
 
 ggplot()+
-  geom_sf(data = domain_sel, aes(fill = E0_v))+ #E0_v
+  geom_sf(data = domain_years_sel, aes(fill = E0_diff_level))+ #
+  scale_fill_manual(values = rev(col_br))+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")+
+  ggtitle("% difference R0 diapausing eggs, [2017-2023] vs [2006-2016]")
+# + scale_fill_gradient(trans = "log")
+
+ggplot()+
+  geom_sf(data = domain_years_sel, aes(fill = E0_v))+ #E0_v
   scale_fill_gradient(trans = "log")+
   geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
 
-
+#########################
 #plot pop
 Sim_m_df = data.frame("variable" = rep(c("E", "J", "I", "A", "E_d"), each = n_r*max(DOS_sim)),
                       "region" = rep(rep(regions, each = max(DOS_sim)), 5),
