c290bda24bb1bda9f375ce54275e475193b6986c
radiciandrea <andrea.radici93@libero.it>
2024-07-11 15:10:47 +0200

    specified funtion fo two codes

--- MM_pis_matrix_EOBS_cycle_consec_plot.R
+++ MM_pis_matrix_EOBS_cycle_consec_plot.R
@@ -1,5 +1,7 @@
 # plot del cycle
-# ModelMetelmann_pis_matrix_EOBS_cycle.R
+# MM_pis_matrix_EOBS_cycle_consec_plot.R
+
+#per plottare anni consecutivi
 
 
 rm(list = ls())
--- ModelMetelmann_pis_matrix_EOBS_cycle_plot.R
+++ ModelMetelmann_pis_matrix_EOBS_cycle_plot.R
@@ -1,6 +1,8 @@
 # plot del cycle
 # ModelMetelmann_pis_matrix_EOBS_cycle.R
 
+#per plottare anni indipendenti
+
 
 rm(list = ls())
 
