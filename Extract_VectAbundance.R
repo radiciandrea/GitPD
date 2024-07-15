041c6ef66e0536446e2b70776a42f20af406777f
radiciandrea <andrea.radici93@libero.it>
2024-07-15 14:30:49 +0200

    loaded data

--- Extract_VectAbundance.R
+++ Extract_VectAbundance.R
@@ -1,1 +1,16 @@
-# code to extract abundances from VectAbundance
+# code to extract abundances from VectAbundance (Da Re et al)
+
+#https://ecoevorxiv.org/repository/view/6444/
+
+rm(list = ls())
+
+library(readxl)
+library(dplyr)
+
+
+folder_eobs = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/VectAbundance/"
+
+#read excel
+data <- read_excel(path = paste0(folder_eobs, "Vectabundace_v015.xlsx"))
+
+
