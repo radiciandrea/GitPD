b915fac90d7d42641201fd124583286e021f7b08
radiciandrea <andrea.radici93@libero.it>
2024-07-16 15:06:34 +0200

    load Datum and write shp. check CRS

--- Extract_Kramer.R
+++ Extract_Kramer.R
@@ -15,25 +15,41 @@ folder_in = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Kra
 
 data = read.csv2(paste0(folder_in , "aegypti_albopictus.csv"), sep = ",")
 
+data$lat<-as.numeric(data$Y)
+data$lon<-as.numeric(data$X)
+
 #load sf domain
 domain <- st_read(paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/domain_sel_", name, ".shp")) 
 
-domain <- domain %>%
-  arrange(region)
+lat_top <- max(domain$top)
+lat_bot <- min(domain$bottom)
+lon_right <- max(domain$right)
+lon_left <- min(domain$left)
 
-# in cycle 
-
-lat_top <- domain$top
-lat_bot <- domain$bottom
-lon_right <- domain$right
-lon_left <- domain$left
-
-#remove obs outside 
-data_geo_sel <- data_geo %>%
-  filter(longitude<max(lon_right))%>%
-  filter(longitude>min(lon_left))%>%
-  filter(latitude<max(lat_top))%>%
-  filter(latitude>min(lat_bot))
-
+#remove obs outside; #remove other vector # keep only points
 data_sel <- data %>%
-  filter(VECTOR == "Aedes albopictus")
+  filter(lon< lon_right) %>%
+  filter(lon>lon_left) %>%
+  filter(lat<lat_top) %>%
+  filter(lat>lat_bot) %>%
+  filter(VECTOR == "Aedes albopictus") %>%
+  filter(LOCATION_TYPE == "point")
+
+data_Albo <- data %>%
+  filter(VECTOR == "Aedes albopictus") %>%
+  filter(LOCATION_TYPE == "point")
+
+
+# WGS1984 Datum
+# https://epsg.io/4326
+Albo_W_EU <- st_as_sf(x = data_sel,
+                      coords = c("lon", "lat"),
+                      crs = "4326")
+
+Albo <- st_as_sf(x = data_Albo,
+                      coords = c("lon", "lat"),
+                      crs = "4326")
+
+st_write(Albo_W_EU, paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/Kramer_2015_Albo_W_EU.shp"))
+
+st_write(Albo, paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/Kramer_2015_Albo.shp"))
