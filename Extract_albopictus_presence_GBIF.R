8266144f486297c221ecad69cf34214bf88d306e
radiciandrea <andrea.radici93@libero.it>
2024-07-19 13:27:12 +0200

    loaded correct data GBIF

--- Extract_albopictus_presence_GBIF.R
+++ Extract_albopictus_presence_GBIF.R
@@ -16,11 +16,20 @@ folder_in = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/GBI
 # little proble: this is identic to Kramer
 # data = read.csv2(paste0(folder_in , "albopictus_presence_GBIF.csv"), sep = ",")
 
-# little problem: more columns than names. preprocessing by hand
-data = read.csv2(paste0(folder_in , "albopictus_AIMSurv_updated_GBIF_mod.csv"))
+# little problem: more columns than names. preprocessing by hand (text to column)
+#data = read.csv2(paste0(folder_in , "albopictus_AIMSurv_updated_GBIF_mod.csv"))
+
+#coordinats are weak: download from gbif website - preprocessing by hand (text to column; cooridnates to number
+data = read.csv2(paste0(folder_in , "download_GBIF_mod.csv"))
 
 data$lat<-as.numeric(data$decimalLatitude)
 data$lon<-as.numeric(data$decimalLongitude)
+
+#filter missing values
+data_Albo <- data %>%
+  filter(species == "Aedes albopictus") %>%
+  filter(is.na(lon) == F) %>%
+  filter(is.na(lat) == F)
 
 #load sf domain
 domain <- st_read(paste0("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/domain_sel_", name, ".shp")) 
@@ -31,17 +40,11 @@ lon_right <- max(domain$right)
 lon_left <- min(domain$left)
 
 #remove obs outside; #remove other vector # keep only points
-data_W_EU <- data %>%
+data_W_EU <- data_Albo %>%
   filter(lon< lon_right) %>%
   filter(lon>lon_left) %>%
   filter(lat<lat_top) %>%
-  filter(lat>lat_bot) %>%
-  filter(species == "Aedes albopictus")
-
-data_Albo <- data %>%
-  filter(species == "Aedes albopictus") %>%
-  filter(is.na(lon) == F) %>%
-  filter(is.na(lat) == F)
+  filter(lat>lat_bot) 
 
 # WGS1984 Datum
 # https://epsg.io/4326
@@ -53,6 +56,10 @@ Albo <- st_as_sf(x = data_Albo,
                  coords = c("lon", "lat"),
                  crs = 4326)
 
-st_write(Albo_W_EU, "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/GBIF_Albo_W_EU.shp")
+#plot
+ggplot()+
+  geom_sf(data = Albo)
 
-st_write(Albo, "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/GBIF_Albo.shp")
+st_write(Albo_W_EU, "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/GBIF_dwnl_Albo_W_EU.shp")
+
+st_write(Albo, "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/GBIF_dwnl_Albo.shp")
