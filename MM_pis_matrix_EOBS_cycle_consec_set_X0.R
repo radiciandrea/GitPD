95c35ef3c62268101595fb17fec2e74113ab64c2
radiciandrea <andrea.radici93@libero.it>
2024-08-02 10:30:13 +0200

    script to set initial conditions for diapausing eggs in W E (d_E after 10 years of running)

---
+++ MM_pis_matrix_EOBS_cycle_consec_set_X0.R
@@ -0,0 +1,28 @@
+# set initial condition X0 for  MM_pis_matrix_EOBS_cycle_consec
+
+# load previous 10-years-later simulation from MM_pis_matrix_EOBS_cycle_consec_plot
+
+rm(list = ls())
+
+library(dplyr)
+library(pracma)
+
+folder_out = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/EOBS_sim_consec"
+folder_in = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Data_sim"
+
+# name = "W_EU"
+# year = 2005
+
+files = list.files(folder_out)
+
+name = substring(files[1], 10, 13)
+years = substring(files, 15, 18)
+
+#carichiamo 1 per le dimensioni
+load(paste0(folder_out, "/", files[which(years == min(as.numeric(years))+10)]))
+
+#correct NAs and < 1
+E0_v[which(E0_v<1)] = 1
+E0_v[which(is.na(E0_v))] = 1
+
+save(E0_v, file = paste0(folder_in, "/X0_E0_consec_W_EU.RData"))
