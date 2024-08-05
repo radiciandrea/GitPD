75ef467559ddc7908cdd2f4e7d90c3c6cd1f32fc
radiciandrea <andrea.radici93@libero.it>
2024-08-05 17:25:53 +0200

    modifid entire plots

--- MM_pis_matrix_EOBS_cycle_consec_geo.R
+++ MM_pis_matrix_EOBS_cycle_consec_geo.R
@@ -43,56 +43,108 @@ A0_m = matrix(NA, ncol = n_r, nrow = length(files))
 for (i in 1:length(files)){
   file = files[i]
   load(paste0(folder_out, "/", file))
+  A_sim = Sim[,1+(1+n_r*3):(4*n_r)]
+  A_sim_pp = A_sim/matrix(rep(people_ha, nrow(A_sim)), nrow = nrow(A_sim), byrow = T)
   
-    rm(Sim)
+  #remove Inf and NaN: set them as 0
+  A_sim_pp[which(is.finite(A_sim_pp)==F)] = 0
+  
+  F0_m[i,] = apply(A_sim_pp, 2, function(x){which(x>1)[1]})
+  S0_m[i,] = apply(A_sim_pp, 2, function(x){sum(x>1)})
+  A0_m[i,] = apply(A_sim_pp, 2, function(x){mean(x[which(x>1)])})
 }
 
-# #Metelamnn, geometric mean. = exp(mean(log))
-# years_sel_1 = 2006:2016 # # 2006:2016
-# E0_m_c_sel_1 <- apply(E0_m[which(years %in% years_sel_1),], 2,
-#                       function(x){x[which(is.na(x))] = exp(mean(log(x[which(is.na(x)==F)]))); return(x)})
-# E0_sel_1 = apply(E0_m_c_sel_1, 2,
-#                  function(x){exp(mean(log(x)))})
-# 
-# years_sel_2 = 2017:2023 # 2017:2023 
-# E0_m_c_sel_2 <- apply(E0_m[which(years %in% years_sel_2),], 2,
-#                       function(x){x[which(is.nan(x))] = exp(mean(log(x[which(is.nan(x)==F)]))); return(x)})
-# E0_sel_2 = apply(E0_m_c_sel_2, 2,
-#                  function(x){exp(mean(log(x)))})
-# 
-# E0_diff = (E0_sel_2 - E0_sel_1)/E0_sel_1
-# 
-# # to plot
-# 
-# years_sel = years_sel_2
-# E0_sel = E0_sel_2
-# 
-# br = c(0, 10^(-3:3), 10^10)
-# br_diff = c(-10^c(2, log(50,10), 0), 0, 10^(0:3), 10^10)
-# 
-# #green-yellow-red-purple
-# col_br= c("#007917", "#65992E", "#8FB338", "#E2CF4D", "#F89061", "#F96970", "#F97ADC", "#A494FB")
-# 
-# #Metelmann map
-# col_br= c("#384AB4", "#5570DF", "#8EB0FE", "#C5D7F3", "#F2CDBB", "#F29878", "#D04B45", "#B00026")
-# 
-# domain_years_sel <- domain_sel%>%
-#   arrange(region) %>%
-#   mutate(E0 = E0_sel)%>%
-#   mutate(E0_level=cut(E0, breaks=br,
-#                       labels=sapply(br[-length(br)], function(x){paste0(">", as.character(x))}))) %>%
-#   mutate(E0_level=factor(as.character(E0_level), levels=rev(levels(E0_level)))) %>%
-#   mutate(E0_diff = E0_diff)%>%
-#   mutate(E0_diff_level=cut(E0_diff, breaks=br_diff,
-#                            labels=sapply(br_diff[-length(br_diff)], function(x){paste0(">", as.character(x))}))) %>%
-#   mutate(E0_diff_level=factor(as.character(E0_diff_level), levels=rev(levels(E0_diff_level))))
-# 
-# if (name == "France") {
-#   regions_sh <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_adm/regions_2015_metropole_region.shp")
-# } else if (name == "Occitanie") {
-#   regions_sh <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_adm/regions_2015_metropole_region.shp")
-#   regions_sh <- regions_sh %>%
-#     filter(Region == "Languedoc-Roussillon et Midi-P")
-# } else if (name == "W_EU") {
-#   regions_sh <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_adm/W_EU_s.shp")
-# }
+# mean plots over 2 periods
+
+# we should correct 2 problems: NaN; Inf; 
+
+years_sel_1 = 2006:2016 # # 2006:2016
+F0_sel_1 = apply(F0_m[years %in% years_sel_1,], 2, function(x){mean(x, na.rm = T)})
+S0_sel_1 = apply(S0_m[years %in% years_sel_1,], 2, function(x){mean(x, na.rm = T)})
+A0_sel_1 = apply(A0_m[years %in% years_sel_1,], 2, function(x){mean(x, na.rm = T)})
+
+years_sel_2 = 2017:2023 # 2017:2023
+F0_sel_2 = apply(F0_m[years %in% years_sel_2,], 2, function(x){mean(x, na.rm = T)})
+S0_sel_2 = apply(S0_m[years %in% years_sel_2,], 2, function(x){mean(x, na.rm = T)})
+A0_sel_2 = apply(A0_m[years %in% years_sel_2,], 2, function(x){mean(x, na.rm = T)})
+
+# to plot
+
+if (name == "France") {
+  regions_sh <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_adm/regions_2015_metropole_region.shp")
+} else if (name == "Occitanie") {
+  regions_sh <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_adm/regions_2015_metropole_region.shp")
+  regions_sh <- regions_sh %>%
+    filter(Region == "Languedoc-Roussillon et Midi-P")
+} else if (name == "W_EU") {
+  regions_sh <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_adm/W_EU_s.shp")
+}
+
+# beginning
+ggplot()+
+  geom_sf(data = domain_sel, aes(fill = F0_sel_1), colour = NA)+ #
+  ggtitle("beginning seasonal activity (date ratio mosquito/person > 1), period 2006-2016")+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
+
+ggplot()+
+  geom_sf(data = domain_sel, aes(fill = F0_sel_2), colour = NA)+ #
+  ggtitle("beginning seasonal activity, period 2017-2023")+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
+
+ggplot()+
+  geom_sf(data = domain_sel, aes(fill = F0_sel_1 - F0_sel_2), colour = NA)+ #
+  scale_fill_gradient2(
+    low = "#5570DF",
+    mid = "white",
+    high = "#D04B45",
+    midpoint = 0,
+    na.value = "grey50")+
+  ggtitle("anticipation seasonal activity, period 10's vs 20's ")+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
+
+
+# length
+ggplot()+
+  geom_sf(data = domain_sel, aes(fill = S0_sel_1), colour = NA)+ #
+  ggtitle("length seasonal activity (#days ratio mosquito/person > 1), period 2006-2016")+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
+
+ggplot()+
+  geom_sf(data = domain_sel, aes(fill = S0_sel_2), colour = NA)+ #
+  ggtitle("length seasonal activity, period 2017-2023")+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
+
+ggplot()+
+  geom_sf(data = domain_sel, aes(fill = S0_sel_2 - S0_sel_1), colour = NA)+ #
+  scale_fill_gradient2(
+    low = "#5570DF",
+    mid = "white",
+    high = "#D04B45",
+    midpoint = 0,
+    na.value = "grey50")+ #
+  ggtitle("variation length seasonal activity, period 10's vs 20's ")+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
+
+# abundance
+ggplot()+
+  geom_sf(data = domain_sel, aes(fill = A0_sel_1), colour = NA)+ #
+  scale_fill_continuous(trans = "log")+
+  ggtitle("mosquito abundance per person (during M/A>0), period 2006-2016")+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
+
+ggplot()+
+  geom_sf(data = domain_sel, aes(fill = A0_sel_2), colour = NA)+ #
+  scale_fill_continuous(trans = "log")+
+  ggtitle("mosquito abundance per person, period 2017-2023")+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
+
+ggplot()+
+  geom_sf(data = domain_sel, aes(fill = A0_sel_2 - A0_sel_1), colour = NA)+ #
+  scale_fill_gradient2(
+    low = "#5570DF",
+    mid = "white",
+    high = "#D04B45",
+    midpoint = 0,
+    na.value = "grey50")+ #
+  ggtitle("variation mosquito abundance per person, period 10's vs 20's")+
+  geom_sf(data = regions_sh, alpha = 0, colour = "grey90")
