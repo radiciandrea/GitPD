7d070d23db993607a4846a1f7b924aff51a99e7d
radiciandrea <andrea.radici93@libero.it>
2024-09-25 16:09:59 +0200

    File created, copied from MM_pis_matrix_EOBS_cycle_consec_compute_plot_R0

---
+++ MM_pis_matrix_EOBS_cycle_consec_compute_plot_R0_StatTest.R
@@ -0,0 +1,111 @@
+# plot del cycle
+# MM_pis_matrix_EOBS_cycle_consec.R
+
+#per plottare anni consecutivi + calcolare R0
+
+rm(list = ls())
+
+library(ggplot2)
+library(reshape2) 
+library(dplyr)
+library(pracma)
+library(sf)
+library(lubridate)
+
+folder_out = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/EOBS_sim_consec"
+folder_eobs = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/EOBS_elab"
+
+files = list.files(folder_out, pattern = ".RData")
+
+name = substring(files[1], 10, 13)
+years = substring(files, 15, 18)
+
+first_day = as.Date(paste0(min(years), "-01-01"))
+last_day = as.Date(paste0(max(years), "-12-31"))
+
+date_sim = seq(from = first_day, to = last_day, by = 'day')
+DOS_sim = 1:length(date_sim)
+n_d = length(DOS_sim)
+
+#carichiamo 1 per le dimensioni
+load(paste0(folder_out, "/", files[1]))
+
+n_c = 5 # numero di classi
+n_r = (ncol(Sim)-1)/n_c #numero di regioni
+regions = 1:n_r
+
+#carichiamo 1 per la popolazion
+load(paste0(folder_eobs, "/EOBS_sel_", years[1], "_", name, ".RData")) #EOBS W_EU #Occitanie #France
+
+# human hosts
+H_v <- W_tot_df %>% 
+  distinct(region, .keep_all = TRUE) %>%
+  pull("pop")
+
+# R0s Dengue
+
+R0_DG_tot = matrix(NA, ncol = n_r, nrow = n_d) 
+R0_DG_m = matrix(NA, ncol = n_r, nrow = length(files))
+
+k = 0
+for (i in 1:length(files)){
+  file = files[i]
+  year = years[i]
+  
+  load(paste0(folder_out, "/", file))
+  n_d_i = nrow(Sim)
+  
+  #Getting weather from EOBS
+  load(paste0(folder_eobs, "/EOBS_sel_", year, "_", name, ".RData")) #EOBS W_EU #Occitanie #France
+  temp = matrix(W_tot_df$T_av, nrow = n_d_i)
+  
+  #bio
+  Adults = Sim[, 1+(n_r*3+1):(n_r*4)]*100 #per km2
+  H_m =   matrix(rep(H_v , n_d_i), nrow = n_d_i, byrow = T )
+  m = Adults/H_m # adult female osquito to human ration
+  
+  #parameters
+  mu_A = -log(0.677 * exp(-0.5*((temp-20.9)/13.2)^6)*temp^0.1) # adult mortality rate
+  mu_A[which(temp<=0)] = -log(0.677 * exp(-0.5*((temp[which(temp<=0)]-20.9)/13.2)^6))  #correct the problems due to negative values from SI
+  
+  #EIP
+  EIP_DN_Bnk = 0.11*temp^2 - 7.13*temp +121.17 #Benkimoun 2021
+  EIP_DN_Mtl = 1.03*(4*exp(5.15 - 0.123*temp)) #Metelmann 2021
+
+  #choice EIP
+  EIP_DG = EIP_DN_Mtl
+  
+  #Vectorial competence
+  B = pmax(0,-0.0043*temp^2 + 0.2593*temp - 3.2705) #Benkimoun 2021
+  b_v2H = 0.5 # b Blagrove 2020
+  
+  #to check this:
+  b_H2v_DG_Mtl = 0.31 # beta Mtl 2021
+  
+  #ch
+  b_H2v_DG = b_H2v_DG_Mtl
+  
+  IIP_DG = 5 #Benkimoun 2021, 
+
+  a = (0.0043*temp + 0.0943)/2  #biting rate (Zanardini et al., Caminade 2016, Blagrove 2020)
+  
+  #host preference
+  phi_a_U = 0.9 #human biting preference (urban)
+  phi_a_R = 0.5 #human biting preference (rural) #Caminade 2016
+  
+  #choose
+  R_th = 50 #defines density over km2 below which an area is "rural", with little phi
+  phi_a = phi_a_U*(H_m>R_th)+phi_a_R*(H_m<=R_th)
+  
+  # VC = (a*phi_a)^2*m*exp(-mu_A*EIP)/mu_A #Vector capacity as RossMcDonald
+  R0_DG = (a*phi_a)^2*m/(mu_A+mu_A^2*EIP_DG)*b_v2H*b_H2v_DG*IIP_DG # as Zanardini et al.
+  
+  n_d_i = nrow(R0_ZK)
+  R0_DG_tot[k + 1:n_d_i,]=R0_DG
+  k = k + n_d_i
+  R0_DG_m[i,] = colSums(R0_DG>1)
+}
+
+R0_tot = R0_DG_tot
+R0_m = R0_DG_m
+
