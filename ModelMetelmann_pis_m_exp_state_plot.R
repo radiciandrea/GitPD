6028577aae2de4e41ec787261966bcabf2c9d613
radiciandrea <andrea.radici93@libero.it>
2024-10-28 11:49:28 +0100

    added cities, modified plots

--- ModelMetelmann_pis_m_exp_state_plot.R
+++ ModelMetelmann_pis_m_exp_state_plot.R
@@ -31,15 +31,15 @@ cities = c("Montpellier", "Paris", "Paris_S", "Madrid", "Rome_E",
 
 cities = c("Coimbra", "Montpellier", "Madrid", "Lugano", "Frankfurt aum Main", "Paris", "Innsbruck", "Antwerp", "London_N")
 
-years_eval = c(2003, 2008, 2013, 2018, 2023)
-years = 2000:2023
+cities = c("Montpellier", "Bilbao", "Augsburg", "Paris-centre", "Paris-suburbs", "Paris-region")
 
+years_eval = 2004:2023
+years = 2000:2023
 
 #MULTIPLICATIVE
 folder_plot = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Esperimenti/Outputs/Exposure_state_mol/"
 # #or
 # folder_plot = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Esperimenti/Outputs/Exposure_state_pow/"
-
 
 for (city_x in cities){
   
@@ -57,129 +57,100 @@ for (city_x in cities){
   for(y in years_eval){
     
     point_df$T_av_summer[which(point_df$year == y)] <- W_tot_df %>% 
-      filter(year %in% c((y-3):y)) %>%
+      filter(year %in% c((y-4):y)) %>%
       filter(DOY >= 121) %>% #1st may
-      filter(DOY < 274) %>% #1st of october
-      dplyr::summarize(T_av_m = mean(T_av)) %>%
-      pull(T_av_m)
+      filter(DOY < 304) %>% #1st of november
+      dplyr::summarize(T_av_summer = mean(T_av)) %>%
+      pull(T_av_summer)
     
     point_df$P_summ_tot[which(point_df$year == y)] <- W_tot_df %>% 
-      filter(year %in% c((y-3):y)) %>%
+      filter(year %in% c((y-4):y)) %>%
       filter(DOY >= 121) %>% #1st may
-      filter(DOY < 274) %>% #1st of october
-      dplyr::summarize(P_summ_tot = sum(P)/length(years)) %>%
+      filter(DOY < 304) %>% #1st of november
+      dplyr::summarize(P_summ_tot = sum(P)/length(c((y-4):y))) %>%
       pull(P_summ_tot)
   }
-
   
-  #Ad
-  breaks_E0 = c(0, 8)
-  
-  # g1 <- ggplot() +
-  #   geom_contour_filled(data = Ind_df, aes(x = T_av_summer, y = P_summ_tot, z = Ad ), breaks = breaks_Ad) +
-  #   ggtitle(paste0("Average log adults/ha (May to Sept) in ", city_x))+
-  #   theme_test()+ 
-  #   geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "black") +
-  #   geom_path(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "black") +
-  #   geom_label_repel(data = point_df, aes(x = T_av_summer, y = P_summ_tot, label = year),
-  #                    label.padding = 0.15) #size = 4
+  #E0
+  breaks_E0 = c(10^(-7), 10^7)
   
   g0_c <- ggplot()+
     geom_contour_fill(data = Ind_df,
-                      aes(x = T_av_summer, y = P_summ_tot, z = E0))+
-    scale_fill_viridis_c(limits = c(min(breaks_E0), max(breaks_E0)),
-                         na.value = "#32003C")+
-    ggtitle(paste0("Average E0~, ", city_x))+
+                      aes(x = T_av_summer, y = P_summ_tot, z = log10(E0)))+
+    scale_fill_viridis_c(limits = log10(c(min(breaks_E0), max(breaks_E0))),
+                         na.value = "#32003C", option = "plasma")+
+    ggtitle(paste0("Average E0, ", city_x))+
     geom_contour(data = Ind_df, aes(x = T_av_summer, y = P_summ_tot, z = E0),
-                 color = "red", breaks = c(1))+
+                 color = "black", breaks = c(1))+
     theme_test()+
-    geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
     geom_path(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
-    geom_label_repel(data = point_df, aes(x = T_av_summer, y = P_summ_tot, label = year),
+    geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot, color = as.factor(year)), size = 2) +
+    guides(size = "legend", colour = "none")+
+    scale_color_grey()+
+    geom_label_repel(data = point_df %>% filter(year ==  2004 | year ==  2023), aes(x = T_av_summer, y = P_summ_tot, label = year),
                      label.padding = 0.15) #size = 4
-
+  
   
   #Ad
-  breaks_Ad = seq(3, 30000, by = 500)
-  
-  # g1 <- ggplot() +
-  #   geom_contour_filled(data = Ind_df, aes(x = T_av_summer, y = P_summ_tot, z = Ad ), breaks = breaks_Ad) +
-  #   ggtitle(paste0("Average log adults/ha (May to Sept) in ", city_x))+
-  #   theme_test()+ 
-  #   geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "black") +
-  #   geom_path(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "black") +
-  #   geom_label_repel(data = point_df, aes(x = T_av_summer, y = P_summ_tot, label = year),
-  #                    label.padding = 0.15) #size = 4
+  breaks_Ad = seq(0.03, 30000, by = 500)
   
   g1_c <- ggplot()+
     geom_contour_fill(data = Ind_df,
                       aes(x = T_av_summer, y = P_summ_tot, z = log10(Ad)))+
     scale_fill_viridis_c(limits = c(min(log10(breaks_Ad)), max(log10(breaks_Ad))),
-                         na.value = "#32003C")+
-    ggtitle(paste0("Average log10 adults/ha (May to Sept)"))+
+                         na.value = "#32003C", option = "mako")+
+    ggtitle(paste0("Average log10 adults/ha (May to Oct), " , city_x))+
     theme_test()+
-    geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
     geom_path(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
-    geom_label_repel(data = point_df, aes(x = T_av_summer, y = P_summ_tot, label = year),
+    geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot, color = as.factor(year)), size = 2) +
+    guides(size = "legend", colour = "none")+
+    scale_color_grey()+
+    geom_label_repel(data = point_df %>% filter(year ==  2004 | year ==  2023), aes(x = T_av_summer, y = P_summ_tot, label = year),
                      label.padding = 0.15) #size = 4
   
-  #R0
-  breaks_R = c(0, 0.5, 1, 2, 4, 7, 10)
-  
-  # g2 <- ggplot()+
-  #   geom_contour_filled(data = Ind_df, aes(x = T_av_summer, y = P_summ_tot, z = R0), breaks = breaks_R)+
+  # #R0
+  # breaks_R = c(0, 0.5, 1, 2, 4, 7, 10)
+  # 
+  # g2_c <- ggplot()+
+  #   geom_contour_fill(data = Ind_df,
+  #                     aes(x = T_av_summer, y = P_summ_tot, z = R0))+
   #   geom_contour(data = Ind_df, aes(x = T_av_summer, y = P_summ_tot, z = R0),
   #                color = "red", breaks = c(1))+
-  #   ggtitle(paste0("Average R0 (May to Sept)"))+
-  #   theme_test()+ 
-  #   geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "black") +
-  #   geom_path(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "black") +
-  #   geom_label_repel(data = point_df, aes(x = T_av_summer, y = P_summ_tot, label = year),
-  #                    label.padding = 0.15) #size = 4
-  
-  g2_c <- ggplot()+
-    geom_contour_fill(data = Ind_df,
-                      aes(x = T_av_summer, y = P_summ_tot, z = R0))+
-    geom_contour(data = Ind_df, aes(x = T_av_summer, y = P_summ_tot, z = R0),
-                 color = "red", breaks = c(1))+
-    scale_fill_viridis_c(limits = c(min(breaks_R), max(breaks_R)),
-                         na.value = "#32003C")+
-    ggtitle("Average R0 (May to Sept)")+
-    theme_test()+
-    geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
-    geom_path(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
-    geom_label_repel(data = point_df, aes(x = T_av_summer, y = P_summ_tot, label = year),
-                     label.padding = 0.15) #size = 4
-  
-  
-  breaks_nR = c(0, 1, 5, 10, 20, 50, 90, 150)
-  
-  # g3 <- ggplot()+
-  #   geom_contour_filled(data = Ind_df, aes(x = T_av_summer, y = P_summ_tot, z = nR0), breaks = breaks_nR)+
-  #   ggtitle(paste0("n days with R0 >1"))+
+  #   scale_fill_viridis_c(limits = c(min(breaks_R), max(breaks_R)),
+  #                        na.value = "#32003C")+
+  #   ggtitle("Average R0 (May to Oct)")+
   #   theme_test()+
-  #   geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "black") +
-  #   geom_path(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "black") +
+  #   geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
+  #   geom_path(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
   #   geom_label_repel(data = point_df, aes(x = T_av_summer, y = P_summ_tot, label = year),
   #                    label.padding = 0.15) #size = 4
+  
+  
+  breaks_nR = c(0, 1, 5, 10, 20, 40, 80, 120, 170)
   
   g3_c <- ggplot()+
     geom_contour_fill(data = Ind_df,
                       aes(x = T_av_summer, y = P_summ_tot, z = nR0))+
     scale_fill_viridis_c(limits = c(min(breaks_nR), max(breaks_nR )),
                          na.value = "#32003C")+
-    ggtitle("n days with R0 >1")+
+    ggtitle(paste0("n days with R0 >1, ", city_x))+
     geom_contour(data = Ind_df, aes(x = T_av_summer, y = P_summ_tot, z = nR0),
                  color = "red", breaks = c(1))+
     theme_test()+
-    geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
     geom_path(data = point_df, aes(x = T_av_summer, y = P_summ_tot), color= "white") +
-    geom_label_repel(data = point_df, aes(x = T_av_summer, y = P_summ_tot, label = year),
+    geom_point(data = point_df, aes(x = T_av_summer, y = P_summ_tot, color = as.factor(year)), size = 2) +
+    guides(size = "legend", colour = "none")+
+    scale_color_grey()+
+    geom_label_repel(data = point_df %>% filter(year ==  2004 | year ==  2023), aes(x = T_av_summer, y = P_summ_tot, label = year),
                      label.padding = 0.15) #size = 4
   
   
   # Save
   g_tot <- ggarrange(g0_c, g1_c, g3_c, ncol = 1)
+  
+  ggsave(paste0(folder_plot, "NEW_g", city_x ,"_E0.png"), g0_c, units="in", height=8/3, width= 5.5, dpi=300)
+  ggsave(paste0(folder_plot, "NEW_g", city_x ,"_Ad.png"), g1_c, units="in", height=8/3, width= 5.5, dpi=300)
+  ggsave(paste0(folder_plot, "NEW_g", city_x ,"_nR.png"), g3_c, units="in", height=8/3, width= 5.5, dpi=300)
   
   ggsave(paste0(folder_plot, "NEW_g", city_x ,".png"), g_tot, units="in", height=8, width= 5.5, dpi=300)
 }
@@ -193,7 +164,7 @@ for (city_x in cities){
 
 
 
-# VARIANCE-BASED
+# VARIANCE-BASED (TO BE UPTDATED)
 
 folder_plot = "C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Esperimenti/Outputs/Exposure_state_pow/"
 
