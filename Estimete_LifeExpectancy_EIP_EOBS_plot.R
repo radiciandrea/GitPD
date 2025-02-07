ce958fa046a38766ce9610613c6b245bf2ac84c9
radiciandrea <andrea.radici93@libero.it>
2025-02-07 18:26:50 +0100

    added cycle and plot
    investigate effect temperture in southern europe and remove outliers in sweden (why?)

--- Estimete_LifeExpectancy_EIP_EOBS_plot.R
+++ Estimete_LifeExpectancy_EIP_EOBS_plot.R
@@ -17,7 +17,19 @@ folder_shp = ""
 #load weather EOBS for each year: specifications
 type = "01"
 name = "W_EU"
-years = 2005:2023
+years = 2006:2023
+
+#load shp
+domain <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/domain_sel_01_W_EU.shp") %>%
+  arrange(region)
+countries_sh <-  st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_adm/european-countries.shp")
+n_r = nrow(domain)
+
+#initialise matrices
+LE_A_JAS_m = matrix(NA, nrow = length(years), ncol = n_r)
+EIP_JAS_m = matrix(NA, nrow = length(years), ncol = n_r)
+C_m = matrix(NA, nrow = length(years), ncol = n_r)
+CP_JAS_m = matrix(NA, nrow = length(years), ncol = n_r)
 
 for (year in years){
   
@@ -27,7 +39,6 @@ for (year in years){
   #days :regions
   DOY = unique(W_tot_df$DOY)
   n_d = max(DOY)
-  n_r = nrow(W_tot_df)/n_d
   
   temp = matrix(W_tot_df$T_av, nrow = n_d)
   
@@ -48,44 +59,138 @@ for (year in years){
 
   #number of days in which EIP < LE_A (Compatibility with transmission)
   C = colSums(EIP<LE_A)
+  
+  #probability that EIP < LE_A
+  CP = 1-pexp(EIP, rate = mu_A)
 
   #Average EIP and LE_A in months in which there are cases (J-A-S)
   d_i = yday(paste0("01-07-", year))
   d_f = yday(paste0("01-10-", year))
-  LE_A_JAS = colSums(LE_A[d_i:d_f,])/(d_f-d_i+1)
-  EIP_JAS = colSums(EIP[d_i:d_f,])/(d_f-d_i+1)
+  LE_A_JAS = colMeans(LE_A[d_i:d_f,])
+  EIP_JAS = colMeans(EIP[d_i:d_f,])
   
+  #Average (daily) fraction of mosquitoes that can survive EIP
+  #this is independent of mosquito presence
+  CP_JAS = colMeans(CP[d_i:d_f,])
+  
+  LE_A_JAS_m[which(years == year),] = LE_A_JAS 
+  EIP_JAS_m[which(years == year),] = EIP_JAS 
+  C_m[which(years == year),] = C 
+  CP_JAS_m[which(years == year),] = CP_JAS 
   
 }
 
-# to plot
-domain <- st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_elab/domain_sel_01_W_EU.shp") %>%
-  arrange(region)
-countries_sh <-  st_read("C:/Users/2024ar003/Desktop/Alcuni file permanenti/Post_doc/Dati/Shp_adm/european-countries.shp")
+# Average 2006-2014
+years_1 = 2006:2014
 
+LE_A_JAS_1 = colMeans(LE_A_JAS_m[years %in% years_1,])
+EIP_JAS_1 = colMeans(EIP_JAS_m[years %in% years_1,])
+C_1 = colMeans(C_m[years %in% years_1,])
+CP_JAS_1 = colMeans(CP_JAS_m[years %in% years_1,])
 
+g_LE_A_JAS_1 <- ggplot()+
+  geom_sf(data = domain, aes(fill = LE_A_JAS_1), colour = NA)+ #
+  scale_fill_gradient(trans = "log")+
+  geom_sf(data = countries_sh, alpha = 0, colour = "white")+
+  coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
+  ggtitle("")+
+  theme_void()
 
-g1 <- ggplot()+
-  geom_sf(data = domain, aes(fill = C), colour = NA)+ #
+g_EIP_JAS_1 <- ggplot()+
+  geom_sf(data = domain, aes(fill = EIP_JAS_1), colour = NA)+ #
+  scale_fill_gradient(trans = "log")+
+  geom_sf(data = countries_sh, alpha = 0, colour = "white")+
+  coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
+  ggtitle("")+
+  theme_void()
+
+g_C_1 <- ggplot()+
+  geom_sf(data = domain, aes(fill = C_1), colour = NA)+ #
   #scale_fill_manual(values = col_x)+
   geom_sf(data = countries_sh, alpha = 0, colour = "white")+
   coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
-  ggtitle("C")+
+  ggtitle("")+
   theme_void()
 
-g2 <- ggplot()+
-  geom_sf(data = domain, aes(fill = LE_A_JAS), colour = NA)+ #
+g_CP_JAS_1 <- ggplot()+
+  geom_sf(data = domain, aes(fill = CP_JAS_1), colour = NA)+ #
+  geom_sf(data = countries_sh, alpha = 0, colour = "white")+
+  coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
+  ggtitle("")+
+  theme_void()
+
+# Average 2015-2023
+
+years_2 = 2015:2024
+
+LE_A_JAS_2 = colMeans(LE_A_JAS_m[years %in% years_2,])
+EIP_JAS_2 = colMeans(EIP_JAS_m[years %in% years_2,])
+C_2 = colMeans(C_m[years %in% years_2,])
+CP_JAS_2 = colMeans(CP_JAS_m[years %in% years_2,])
+
+g_LE_A_JAS_2 <- ggplot()+
+  geom_sf(data = domain, aes(fill = LE_A_JAS_2), colour = NA)+ #
   scale_fill_gradient(trans = "log")+
   geom_sf(data = countries_sh, alpha = 0, colour = "white")+
   coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
-  ggtitle("C")+
+  ggtitle("")+
   theme_void()
 
-g3 <- ggplot()+
-  geom_sf(data = domain, aes(fill = EIP_JAS), colour = NA)+ #
+g_EIP_JAS_2 <- ggplot()+
+  geom_sf(data = domain, aes(fill = EIP_JAS_2), colour = NA)+ #
   scale_fill_gradient(trans = "log")+
   geom_sf(data = countries_sh, alpha = 0, colour = "white")+
   coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
-  ggtitle("C")+
+  ggtitle("")+
   theme_void()
 
+g_C_2 <- ggplot()+
+  geom_sf(data = domain, aes(fill = C_2), colour = NA)+ #
+  #scale_fill_manual(values = col_x)+
+  geom_sf(data = countries_sh, alpha = 0, colour = "white")+
+  coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
+  ggtitle("")+
+  theme_void()
+
+g_CP_JAS_2 <- ggplot()+
+  geom_sf(data = domain, aes(fill = CP_JAS_2), colour = NA)+ #
+  geom_sf(data = countries_sh, alpha = 0, colour = "white")+
+  coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
+  ggtitle("")+
+  theme_void()
+
+# Average variation
+
+LE_A_JAS_delta = LE_A_JAS_2 - LE_A_JAS_1
+EIP_JAS_delta = EIP_JAS_2 - EIP_JAS_1
+C_delta = C_2 - C_1
+CP_JAS_delta = CP_JAS_2 - CP_JAS_1
+
+g_LE_A_JAS_delta <- ggplot()+
+  geom_sf(data = domain, aes(fill = LE_A_JAS_delta), colour = NA)+ #
+  geom_sf(data = countries_sh, alpha = 0, colour = "white")+
+  coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
+  ggtitle("")+
+  theme_void()
+
+g_EIP_JAS_delta <- ggplot()+
+  geom_sf(data = domain, aes(fill = EIP_JAS_delta), colour = NA)+ #
+  geom_sf(data = countries_sh, alpha = 0, colour = "white")+
+  coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
+  ggtitle("")+
+  theme_void()
+
+g_C_delta <- ggplot()+
+  geom_sf(data = domain, aes(fill = C_delta), colour = NA)+ #
+  #scale_fill_manual(values = col_x)+
+  geom_sf(data = countries_sh, alpha = 0, colour = "white")+
+  coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
+  ggtitle("")+
+  theme_void()
+
+g_CP_JAS_delta <- ggplot()+
+  geom_sf(data = domain, aes(fill = CP_JAS_delta), colour = NA)+ #
+  geom_sf(data = countries_sh, alpha = 0, colour = "white")+
+  coord_sf(xlim = c(-15, 19), ylim = c(36, 60)) +
+  ggtitle("")+
+  theme_void()
